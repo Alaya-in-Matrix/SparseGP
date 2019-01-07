c0f04531da3de7cadd93b0a28d3e8f3d15a7c31a
wenlong lyu <lvwenlong_lambda@qq.com>
2019-01-07 17:44:48 +0800

    Update util

--- util.py
+++ util.py
@@ -6,7 +6,7 @@ def chol(A):
 def chol_solve(L, y):
     lx, _ = torch.trtrs(y,  L, upper = False);
     x, _  = torch.trtrs(lx, L, upper = False, transpose = True);
-    return x
+    return x.reshape(y.shape)
 
 def logDet(L):
     return 2 * L.diag().log().sum()
